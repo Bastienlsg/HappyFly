69ad9e0218bcc7cc510e7ee73b714e7594cfe4f0
Bastienlsg <95766791+Bastienlsg@users.noreply.github.com>
2023-03-29 18:54:00 +0200

    ajout du projet

---
+++ src/main/java/com/project/happy_fly/HappyFlyApplication.java
@@ -0,0 +1,13 @@
+package com.project.happy_fly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyFlyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HappyFlyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/happy_fly/HappyFlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.happy_fly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyFlyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
